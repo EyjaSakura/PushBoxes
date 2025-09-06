0163fddf71b4f822622c717c92b17dac7ff91a45
Eyja_Sakura <1815442184@qq.com>
2025-09-06 21:08:03 +0800

    Damaged Map Test

--- src/common/MapList.cpp
+++ src/common/MapList.cpp
@@ -6,6 +6,9 @@ MapList::MapList(const std::string& folderPath) {
     try {
         for (const auto& entry : fs::directory_iterator(folderPath)) {
             if (entry.is_regular_file() && entry.path().extension() == ".txt") {
+                //测试用，长度为27是因为文件数量少于10，并且需要注意是绝对路径
+                if (entry.path().string().length() > 27) { continue; }
+
                 maps_.push_back(entry.path().string());
             }
         }
